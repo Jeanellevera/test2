295c0b4932823891633ad61b41ea8e96d7760cc1
Jeanellevera <145831198+Jeanellevera@users.noreply.github.com>
2024-11-06 23:08:50 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Uuntu Acivity</title>
+	<title>Ubuntu Server PHP Deployment</title>
 </head>
 <body>
-   <title>JEAN ELLEVERA</title>
+
+
+	<p>Ubuntu Server PHP Deployment</p>
 </body>
+
+<div>
+	<label>Jean</label>
+	<input type="text" name="firstname">
+</div>
+<div>
+	<label>Malinao</label>
+	<input type="text" name="middlename">
+</div>
+<div>
+	<label>Ellevera</label>
+	<input type="text" name="lastname">
+</div>
+
 </html>
